974837087de3cd59e2a2e786e5c0930d3d6f50aa
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2023-06-04 01:03:49 +0900

    Update: postのクラスをエクスポート

---
+++ src/types_py/rest/post/__init__.py
@@ -0,0 +1,9 @@
+from .info import Info
+from .list_creator import ListCreator
+from .paginate_creator import PagenateCreator
+
+__all__ = [
+    "Info",
+    "ListCreator",
+    "PagenateCreator",
+]

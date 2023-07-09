a6e1bf7f8ab502c0d19663c4b94f0f20671debc4
Mijinko_SD <79152329+MijinkoSD@users.noreply.github.com>
2023-07-09 22:23:23 +0900

    Update: UserAgent

--- src/downloader/rest/common.py
+++ src/downloader/rest/common.py
@@ -8,7 +8,8 @@ HEADERS: Final[dict[str, str]] = {
     "accept-language": "ja,en-US;q=0.9,en;q=0.8",
     "origin": "https://www.fanbox.cc",
     "referer": "https://www.fanbox.cc/",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 "
-    "Safari/537.36"
+    "user-agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/114.0.0.0 Safari/537.36")
 }
